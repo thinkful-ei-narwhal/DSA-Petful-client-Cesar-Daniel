deca675dd3c1ecb1e540649e45e967c38ccbc4b8
Cesar Morales <cesarlenin2@live.com>
2020-07-17 07:25:35 -0700

    changed horoku link

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-  API_ENDPOINT: process.env.REACT_APP_API_KEY|| 'https://git.heroku.com/agile-anchorage-08133.git',
+  API_ENDPOINT: process.env.REACT_APP_API_KEY|| 'https://secret-springs-25473.herokuapp.com/',
 };
